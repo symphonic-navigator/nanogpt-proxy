9f2481b0b76d0d4ad45451797801f860181d2051
Patrick Bélanger <1848500+patrickbelanger@users.noreply.github.com>
2025-11-23 01:36:25 -0500

    feature(admin-api): cleansweep

--- apps/admin-api/src/app.module.ts
+++ apps/admin-api/src/app.module.ts
@@ -1,8 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { SecurityModule } from './security/security.module';
@@ -16,7 +13,5 @@ import { SecurityModule } from './security/security.module';
     UsersModule,
     SecurityModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
