5f413765305bd657f73e32ee1681dfafe733cdd7
Patrick Bélanger <1848500+patrickbelanger@users.noreply.github.com>
2025-11-27 23:39:22 -0500

    feature(admin-api): fix eslint

--- apps/proxy/src/main.ts
+++ apps/proxy/src/main.ts
@@ -1,15 +1,12 @@
-import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { EnvironmentService } from '@nanogpt-monorepo/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = app.get(ConfigService);
-
   const environmentService = app.get(EnvironmentService);
 
   await app.listen(environmentService.proxyPort);
 }
 
-bootstrap();
+void bootstrap();
